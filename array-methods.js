e46af3ec5dcda9e1477e7b6e7af22ad58af18058
Bryan Hankins <bryanhankins@me.com>
2016-08-12 07:49:28 -0500

    first objective

--- array-methods.js
+++ array-methods.js
@@ -1,1 +1,6 @@
-console.log("test")
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+planets.forEach(function(planet) {
+console.log("Use the forEach method to add the name of each planet to a div element in your HTML with an id of 'planets': ", planet);
+})
+
