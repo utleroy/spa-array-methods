9ca7ea1a2f15d240f992dac3e8be4dbb0c8a58b8
Bryan Hankins <bryanhankins@me.com>
2016-08-12 08:09:56 -0500

    first objective display in DOM

--- array-methods.js
+++ array-methods.js
@@ -1,6 +1,22 @@
 var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
 
-planets.forEach(function(planet) {
+var output = document.getElementById("planets");
+
+var display = planets.forEach(function(planet) { /*function(planet) is the callback function*/
 console.log("Use the forEach method to add the name of each planet to a div element in your HTML with an id of 'planets': ", planet);
+
+output.innerHTML += planet + " ";
 })
 
+
+
+
+
+
+// Use the map method to create a new array where the first letter of each planet is capitalized
+
+// Use the filter method to create a new array that contains planets with the letter 'e'
+
+// Use the reduce method to create a sentence from the words in the following array
+
+// var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
